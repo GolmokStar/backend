e3d50fbf3097973cfb6f142439f34e5827599c7e
TwooTwoo <mbnman12@gmail.com>
2025-02-28 01:38:09 +0900

    fix : 다이어리 관련 API 포스트맨 테스트 진행 및 코드 수정, 월별 방문기록&일기 조회는 로직 수정 필요

--- src/main/java/com/golmok/golmokstar/controller/DiaryController.java
+++ src/main/java/com/golmok/golmokstar/controller/DiaryController.java
@@ -20,33 +20,68 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    // 다이어리 작성 (생성)
     @PostMapping
     public ResponseEntity<?> createDiary(@RequestBody CreateDiaryRequestDto dto) {
         try {
             CreateDiaryResponseDto response = diaryService.createDiary(dto);
             return ResponseEntity.ok(response);
-        } catch (Exception e) {
+        } catch (ResponseStatusException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "다이어리 생성 내부 로직 오류"));
         }
     }
 
+    // 다이어리 조회 (딘일)
     @GetMapping("{diaryId}")
     public ResponseEntity<?> getDiaryDetail(@PathVariable Long diaryId) {
         try {
             GetDiaryDetailResponseDto response = diaryService.getDiaryDetail(diaryId);
             return ResponseEntity.ok(response);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "단일 다이어리 조회 내부 로직 오류"));
         }
     }
 
+    // 특정 여행에 포함된 다이어리 모두 조회
     @GetMapping("/trip/{tripId}")
     public ResponseEntity<?> getDiariesByTripId(@PathVariable Long tripId) {
         try {
             List<DiaryListResponseDto> response = diaryService.getDiariesByTripId(tripId);
             return ResponseEntity.ok(response);
-        } catch (Exception e) {
+        } catch (ResponseStatusException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "여행별 다이어리 목록 조회 내부 로직 오류"));
+        }
+    }
+
+    // 특정 월 방문 기록 & 일기 조회
+    @GetMapping("/{year}/{month}")
+    public ResponseEntity<?> getMonthlyHistoriesAndDiaries(@PathVariable int year, @PathVariable int month) {
+        try {
+            List<GetMonthlyDiaryHistoriesDto> result = diaryService.getMonthlyDiaryHistoriesByMonth(year, month);
+            return ResponseEntity.ok(result);
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "특정 월 방문 기록 & 일기 조회 내부 로직 오류"));
+        }
+    }
+
+    // 날자 최신순 다이어리 조회 최대 5개
+    @GetMapping("/recent")
+    public ResponseEntity<?> getRecentDiaries() {
+        try {
+            List<GetRecentDiariesResponseDto> result = diaryService.getRecentDiaries();
+            return ResponseEntity.ok(result);
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "최근 다이어리 조회 내부 로직 오류"));
         }
     }
 
@@ -59,29 +94,4 @@ public class DiaryController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
         }
     }
-
-    // 다이어리 목록 날자 최신순으로 조회
-    @GetMapping("/recent")
-    public ResponseEntity<?> getRecentDiaries(@PathVariable Long diaryId) {
-        try {
-            List<GetRecentDiariesResponseDto> result = diaryService.getRecentDiaries();
-            return ResponseEntity.ok(result);
-        } catch (ResponseStatusException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "최근 다이어리 조회 내부 로직 오류"));
-        }
-    }
-
-    // 특정 월 방문 기록 & 일기 조회
-    @GetMapping("/{year}/{month}")
-    public ResponseEntity<?> getMonthlyHistoriesAndDiaries(@PathVariable int year, @PathVariable int month) {
-        try {
-            List<GetMonthlyDiaryHistoriesDto> result = diaryService.getMonthlyDiaryHistoriesByMonth(year, month);
-            return ResponseEntity.ok(result);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "특정 월 방문 기록 & 일기 조회 내부 로직 오류"));
-        }
-    }
-
 }
--- src/main/java/com/golmok/golmokstar/service/DiaryService.java
+++ src/main/java/com/golmok/golmokstar/service/DiaryService.java
@@ -10,15 +10,12 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,27 +25,28 @@ public class DiaryService {
     private final TripRepository tripRepository;
     private final RecordRepository recordRepository;
 
-    // 다이어리 작성(생성)
+    // 다이어리 작성 (생성)
     @Transactional
     public CreateDiaryResponseDto createDiary(CreateDiaryRequestDto dto) {
-        Trip trip = tripRepository.findById(dto.getTripId())
+        Trip trip = tripRepository.findByTripId(dto.getTripId())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 tripId를 찾을 수 없습니다."));
 
         // 빈 Diary 객체 생성한 다음 필드 세팅
         // 유효하지 않은 입력 데이터입니다 예외처리 해야함
-        Diary diary = new Diary();
-        diary.setTrip(trip);
-        diary.setDiaryDate(dto.getDiaryDate());
-        diary.setContent(dto.getContent());
-        diary.setPhoto(dto.getPhoto());
-        diary.setAiDraft(dto.getAiDraft());
+        Diary diary = new Diary(
+                trip,
+                dto.getDiaryDate(),
+                dto.getContent(),
+                dto.getPhoto(),
+                dto.getAiDraft()
+        );
 
         diaryRepository.save(diary);
 
         return new CreateDiaryResponseDto(diary.getDiaryId());
     }
 
-    // 다이어리 조회(단일)
+    // 다이어리 조회 (단일)
     @Transactional //(readOnly = true)
     public GetDiaryDetailResponseDto getDiaryDetail(Long diaryId) {
         Diary diary = diaryRepository.findByDiaryId(diaryId)
@@ -64,8 +62,7 @@ public class DiaryService {
         );
     }
 
-    // 특정 여행의 포함된 다이어리 모두 조회
-    //
+    // 특정 여행에 포함된 다이어리 모두 조회
     @Transactional
     public List<DiaryListResponseDto> getDiariesByTripId(Long tripId) {
         Trip trip = tripRepository.findByTripId(tripId)
@@ -89,35 +86,6 @@ public class DiaryService {
                 .collect(Collectors.toList());
     }
 
-    // 다이어리 삭제
-    @Transactional
-    public DeleteDiaryResponseDto deleteDiary(Long diaryId) {
-        if(!diaryRepository.existsById(diaryId)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 requestId를 찾을 수 없습니다.");
-        }
-
-        diaryRepository.deleteById(diaryId);
-
-        return new DeleteDiaryResponseDto(diaryId);
-    }
-
-    // 날자 최신순 다이어리 조회 최대 5개
-    @Transactional
-    public List<GetRecentDiariesResponseDto> getRecentDiaries() {
-        // 날자 최신순 다이어리 5개 조회하는 쿼리 날리고 리스트로 만들어서 저장
-        List<GetRecentDiariesResponseDto> diaries = diaryRepository.findTop5ByOrderByDiaryDateDesc()
-                .stream()
-                // 반환용 dto 객체로 매핑
-                .map(GetRecentDiariesResponseDto::fromEntity)
-                .toList();
-
-        if(diaries.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "작성된 다이어리가 없습니다.");
-        }
-
-        return diaries;
-    }
-
     // 특정 월 방문 기록 & 일기 조회
     @Transactional
     public List<GetMonthlyDiaryHistoriesDto> getMonthlyDiaryHistoriesByMonth(int year, int month) {
@@ -135,9 +103,42 @@ public class DiaryService {
         diaryRepository.findByDiaryDateBetween(start, end)
                 .forEach(diary -> result.add(GetMonthlyDiaryHistoriesDto.fromDiary(diary)));
 
+        if (result.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "선택한 월에 대한 방문 기록이 없습니다.");
+        }
+
         // 최신순 정렬
         result.sort(Comparator.comparing(GetMonthlyDiaryHistoriesDto::getDate).reversed());
 
         return result;
+    }
+
+    // 날자 최신순 다이어리 조회 최대 5개
+    @Transactional
+    public List<GetRecentDiariesResponseDto> getRecentDiaries() {
+        // 날자 최신순 다이어리 5개 조회하는 쿼리 날리고 리스트로 만들어서 저장
+        List<GetRecentDiariesResponseDto> diaries = diaryRepository.findTop5ByOrderByDiaryDateDesc()
+                .stream()
+                // 반환용 dto 객체로 매핑
+                .map(GetRecentDiariesResponseDto::fromEntityToDto)
+                .toList();
+
+        if(diaries.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "작성된 다이어리가 없습니다.");
+        }
+
+        return diaries;
+    }
+
+    // 다이어리 삭제
+    @Transactional
+    public DeleteDiaryResponseDto deleteDiary(Long diaryId) {
+        if(!diaryRepository.existsById(diaryId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 requestId를 찾을 수 없습니다.");
+        }
+
+        diaryRepository.deleteById(diaryId);
+
+        return new DeleteDiaryResponseDto(diaryId);
     }
 }

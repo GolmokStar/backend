b41180f9fa95a1dcfde8e37cbbc5536e8982be89
onlyoneprivate <pllm098@naver.com>
2025-02-27 17:09:46 +0900

    feat : PlaceService 추가

--- src/main/java/com/golmok/golmokstar/repository/PlaceRepository.java
+++ src/main/java/com/golmok/golmokstar/repository/PlaceRepository.java
@@ -10,4 +10,7 @@ public interface PlaceRepository extends JpaRepository <Place, Long> {
     // 중복 장소 확인 (장소명을 통해 조회)
     Optional<Place> findByPlaceName(String placeName);
 
+    // ✅ googlePlaceId로 장소 조회
+    Optional<Place> findByGooglePlaceId(String googlePlaceId);
+
 }
